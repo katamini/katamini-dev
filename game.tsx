570d41a3d3aedbd129d8551439cc136c21c1fab6
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2025-01-14 21:34:54 +0100

    Update game.tsx

--- game.tsx
+++ game.tsx
@@ -354,7 +354,7 @@ const Game: React.FC = () => {
       );
 
        // Add logic to check if all objects are captured and play a sound
-      if ((smallestObject.userData.size < 1 || gameState.collectedObjects.length === gameObjects.length) && finished === false) {
+      if ((gameState.collectedObjects.length >= gameObjects.length) && finished === false) {
         console.log("Game Completed!", time);
         audioRef.current.pause();
         playRandomSound(['music/effects/01.mp3', 'music/effects/03.mp3', 'music/effects/04.mp3', 'music/effects/05.mp3']);
