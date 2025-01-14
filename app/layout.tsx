2c6717c624ff95997bd474aa21aea619e4719701
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2025-01-14 21:16:13 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Katamini',
+  description: 'Katamini: the game v0',
 }
 
 export default function RootLayout({
@@ -13,6 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" type="image/png" href="https://i.imgur.com/UyA8YJC.png" />
       <body>{children}</body>
     </html>
   )
